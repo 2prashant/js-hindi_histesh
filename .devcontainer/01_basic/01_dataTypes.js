59152b982052d8643c6fe6c19e03a3eaae8c7b81
Prashant <108861396+2prashant@users.noreply.github.com>
2023-12-16 18:17:30 +0000

    data type variables

---
+++ .devcontainer/01_basic/01_dataTypes.js
@@ -0,0 +1,13 @@
+
+//primitive data Type
+// Number =>2 to 53
+//string =>""
+//boolean => true/false
+//null =>standalone value
+//undefined =>
+// symbol =>unique
+
+//object
+
+console.log(typeof undefined);
+console.log(typeof null);
---
+++ .devcontainer/01_basic/01_variables.js
@@ -0,0 +1,21 @@
+const accountId=144553
+let accountEmail="Pra@1234gmail.com"
+var accountPassword="12345" 
+accountcity="banars"  // It is not good use
+let accountstate;
+
+//accountId=34566    // Not allowed
+
+accountEmail="pra656@gmail.com"
+accountPassword="98764"
+accountcity="Lucknow"
+
+/*
+
+Prefer not to use  var
+bacause of issue in block scop and Functional scope
+
+*/
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword, accountcity,accountstate]);
