3db966361bc4d5630e95d8344ba4b72b2d149fa2
Prashant <108861396+2prashant@users.noreply.github.com>
2023-12-16 18:43:27 +0000

    data_type

--- .devcontainer/01_basic/01_dataTypes.js
+++ .devcontainer/01_basic/01_dataTypes.js
@@ -1,14 +1,24 @@
+//"use strict";// treat all JS code as newer version
+//alert(3+3);// we are using nodesJs, not brower 
+
+let name="hitesh"
+let age=18
+let isLoggIn=false
+ 
 
 
 //primitive data Type
 // Number =>2 to 53
+//bigint
 //string =>""
 //boolean => true/false
 //null =>standalone value
 //undefined =>
 // symbol =>unique
 
+//NON premitive data type
 //object
 
+console.log(typeof age);
 console.log(typeof undefined);
 console.log(typeof null);
