350da12d61ab578bbd5e8cc15964aec7bdaec551
Prashant <108861396+2prashant@users.noreply.github.com>
2023-12-17 05:48:46 +0000

    typeConversion

---
+++ .devcontainer/01_basic/01_typeconverion.js
@@ -0,0 +1,57 @@
+let score1="33abc"
+let score2=null
+let score3=undefined
+console.log(typeof score1);
+console.log(typeof(score1));
+console.log(typeof score2)
+console.log(typeof score3)
+
+let valueInNumber1=Number(score1)
+console.log(typeof valueInNumber1);
+console.log(valueInNumber1);
+
+let valueInNumber2=Number(score2)
+console.log(typeof valueInNumber2);
+console.log(valueInNumber2);
+
+let valueInNumber3=Number(score3)
+console.log(typeof valueInNumber3);
+console.log(valueInNumber3);
+
+// 33 => 33
+// "33abc" =>NaN
+// true =>1 ; false =>0
+
+let value1=1
+console.log(value1);
+let value2=""
+ console.log(value2)
+
+let value3="pra"
+console.log(value3)
+
+let value11=Boolean(value1)
+console.log(value11);
+
+let value22=Boolean(value2)
+console.log(value22)
+
+let value33=Boolean(value3)
+console.log(value33)
+
+// 1 => true; 0 =>false
+// "" =>false
+// "hitesh" => true
+
+
+let someNumber=33
+console.log(someNumber);
+console.log(typeof someNumber);
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
