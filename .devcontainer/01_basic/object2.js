2035d868285e29574a9d492736c04ef4318d47e7
Prashant <108861396+2prashant@users.noreply.github.com>
2024-01-06 12:51:53 +0000

    JSON

--- .devcontainer/01_basic/object2.js
+++ .devcontainer/01_basic/object2.js
@@ -17,4 +17,61 @@ const regularUser={
     }
 }
 //console.log(regularUser.fullname)
-console.log(regularUser.fullname.username.firstname)
+//console.log(regularUser.fullname.username.firstname)
+
+const obj1={1:"a", 2:"g"}
+const obj2={2:"h",  3:"b"}
+const obj4={5:"a",2:"b"}
+// const obj3={obj1,obj2}
+//console.log(obj3)
+
+//const obj3=Object.assign({},obj1,obj2,obj4)
+
+const obj3={...obj1,...obj2}
+//console.log(obj3)
+
+const user=[
+    {
+        id:1,
+        email:"h@gmail.com"
+    },
+    {
+        id:1,
+        email:"h@gmail.com"
+    },
+    {
+        id:1,
+        email:"h@gmail.com"
+    },
+]
+//console.log(user[1].email)
+
+//console.log(Object.keys(tinderUser));
+//console.log(Object.values(tinderUser));
+//console.log(Object.entries(tinderUser))
+
+//console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+const course={
+    coursename:"js in hindi",
+    price:"999",
+    courseInstructor:"hitesh"
+}
+// const {courseInstructor}=course
+// console.log(courseInstructor);
+
+
+const {courseInstructor:instructor}=course
+console.log(instructor);
+
+
+{
+      "name":"hitesh",
+      "coursename":"js in hindi",
+      "price":"free"
+}
+[
+    {},
+    {},
+    {}
+]
