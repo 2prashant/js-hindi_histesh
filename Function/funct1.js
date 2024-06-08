18a8177b55381342b28ea85fc9017169ac50ceda
Prashant <108861396+2prashant@users.noreply.github.com>
2024-06-08 01:52:54 +0000

    basic concept related to function

--- Function/funct1.js
+++ Function/funct1.js
@@ -1,5 +1,41 @@
 
 function display()
 {
-    
+       console.log('p') 
+       console.log('r') 
+       console.log('a') 
+       console.log('s') 
+       console.log('h') 
+       console.log('a') 
+       console.log('n') 
+       console.log('t') 
+       console.log("===============")
 }
+//display()
+//display()
+
+function sum(num1,num2)
+{
+    console.log("sum: ",num1+num2)
+    console.log("sum : "+(num1+num2))
+    console.log(`sum: ${num1+num2}`)
+}
+//console.log(sum(2,3))
+
+function print(value)
+{
+    if(value===undefined)
+        {
+            console.log("first Enter name ")
+            return 
+        }
+    return value
+}
+//console.log(print())
+//console.log(print("vishal"))
+
+function collectvalue(var1,var2,var3, ...val)
+{
+    return (var1,var2,var3,val)
+}
+console.log(collectvalue(1,2,3,4,5,6,7,8,9,12,1,3,14,1,5,14,))
