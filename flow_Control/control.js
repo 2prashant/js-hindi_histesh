d1f8a3eb6cfb473f64f5c331662e5e8569ae4d9b
Prashant <108861396+2prashant@users.noreply.github.com>
2024-06-08 10:34:38 +0000

    loop & switch

---
+++ flow_Control/control.js
@@ -0,0 +1,34 @@
+
+// let balance=780
+
+// if(balance < 500)
+//     {
+//         console.log("less than 500");
+//     }
+//     else if(balance < 750)
+//     {
+//         console.log("less than 750");
+
+//     }else if(balance <900)
+//         {
+//             console.log("less than 900");
+//         }
+//         else{
+//             console.log("grater than 900")
+//         }
+      
+// const userLoggedIn =true
+// const debitCard=true
+
+// const loggedInFromEmail=true
+// const loggedInFromgoogle=false
+
+// if(userLoggedIn && debitCard)
+//     {
+//         console.log("allow to buy now")  ;
+//     }
+// if(loggedInFromgoogle || loggedInFromEmail)
+//     {
+//         console.log("User logged in");
+//     }
+
---
+++ flow_Control/switch.js
@@ -0,0 +1,32 @@
+// switch(key){
+//     case value:
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month=34
+switch (month){
+    case 1:
+        {
+            console.log("First month");
+            break;
+        }
+    case 2:
+        {
+            console.log("Second month");
+            break;
+        }
+    case 3:
+        {
+            console.log("Third month");
+            break;
+        }
+    default:
+          {
+              console.log("invalid input");
+              break;
+          }
+    
+}
